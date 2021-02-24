03106cc668518f1f17b295ea50bba5049687ad14
Jamie Cansdale <jcansdale@gmail.com>
2021-02-24 12:23:42 +0000

    Add project files.

---
+++ PublicRepoTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PublicRepoTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
